5f1e4ad8a632c8cddcfdb226d7a1ed72fe2fc97e
Bohdan Buhrak <bohdan0410@gmail.com>
2025-09-09 13:40:33 +0300

    add server part(corrects in models) + search

--- my-frontend/src/App.jsx
+++ my-frontend/src/App.jsx
@@ -8,17 +8,20 @@ function App() {
   const [message, setMessage] = useState("Press button to see message");
 
   const fetchMessage = () => {
-    fetch(`${API_BASE_URL}/random-message`)
-      .then(res => {
-        if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
-        return res.json();
-      })
-      .then(data => setMessage(data.message))
-      .catch(err => {
-        console.error(err);
-        setMessage("Error API: " + err.message);
-      });
-  };
+      fetch(`${API_BASE_URL}/random-message`)
+        .then(res => {
+          if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+          return res.json();
+        })
+        .then(data => {
+          // додаємо ссилку перед повідомленням
+          setMessage(`Ссылка на API: ${API_BASE_URL} | Повідомлення: ${data.message}`);
+        })
+        .catch(err => {
+          console.error(err);
+          setMessage("Error API: " + err.message);
+        });
+    };
 
   return (
     <div style={{ padding: 20, fontFamily: "sans-serif" }}>
