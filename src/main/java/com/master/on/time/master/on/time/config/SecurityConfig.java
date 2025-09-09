1048a202ef22c1eda36569231c2596cb0390f625
Bohdan Buhrak <bohdan0410@gmail.com>
2025-09-09 13:35:51 +0300

    add server part(corrects in models) + search

--- src/main/java/com/master/on/time/master/on/time/config/SecurityConfig.java
+++ src/main/java/com/master/on/time/master/on/time/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of("http://localhost:5173",
-                "enchanting-recreation-production.up.railway.app")); // твій фронтенд
+                "http://enchanting-recreation-production.up.railway.app")); // твій фронтенд
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
