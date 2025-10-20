bc189fdd4568d1eca2ebbafedaa19ceafcb02bb8
Gosudar <Bohdan0410@gmail.com>
2025-10-20 22:12:54 +0300

    fix(cors): handle preflight OPTIONS and header case sensitivity

--- src/main/java/com/master/on/time/master/on/time/controller/TestController.java
+++ src/main/java/com/master/on/time/master/on/time/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.master.on.time.master.on.time.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
 import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,5 +14,16 @@ public class TestController {
     @GetMapping("/random-message")
     public Map<String, String> getRandomMessage() {
         return Map.of("message", "Hello! This is a random message from the backend.");
+    }
+
+    @GetMapping("/headers")
+    public String headers(HttpServletRequest request) {
+        StringBuilder sb = new StringBuilder();
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            sb.append(name).append(": ").append(request.getHeader(name)).append("\n");
+        }
+        return sb.toString();
     }
 }
