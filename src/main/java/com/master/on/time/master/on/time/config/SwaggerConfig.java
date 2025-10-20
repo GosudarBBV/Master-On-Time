995f4e13f987daca8aec206289b14d04a5ab4fa8
Gosudar <Bohdan0410@gmail.com>
2025-10-20 21:44:35 +0300

    fix(cors): handle preflight OPTIONS and header case sensitivity

--- src/main/java/com/master/on/time/master/on/time/config/SwaggerConfig.java
+++ src/main/java/com/master/on/time/master/on/time/config/SwaggerConfig.java
@@ -2,8 +2,10 @@ package com.master.on.time.master.on.time.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +15,8 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenApi() {
         return new OpenAPI()
+                .info(new Info().title("Master On Time API").version("v1"))
+                .addServersItem(new Server().url("https://master-on-time-production.up.railway.app"))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth",
                                 new SecurityScheme()
